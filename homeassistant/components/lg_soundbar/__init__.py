1358c43265454d4cda531b705bc94a6fd7260ef3
MojoManagement <816086+MojoManagement@users.noreply.github.com>
2026-08-25 19:52:10 +0200

    refactor: use one persistent LG soundbar connection

--- homeassistant/components/lg_soundbar/__init__.py
+++ homeassistant/components/lg_soundbar/__init__.py
@@ -1,10 +1,7 @@
 """The lg_soundbar component."""
 
 from homeassistant import config_entries, core
-from homeassistant.const import CONF_HOST, CONF_PORT, Platform
-from homeassistant.exceptions import ConfigEntryNotReady
-
-from .config_flow import async_test_connect
+from homeassistant.const import Platform
 
 PLATFORMS = [Platform.MEDIA_PLAYER]
 
@@ -12,12 +9,7 @@ PLATFORMS = [Platform.MEDIA_PLAYER]
 async def async_setup_entry(
     hass: core.HomeAssistant, entry: config_entries.ConfigEntry
 ) -> bool:
-    """Set up an LG soundbar without waking a device in standby."""
-    try:
-        await async_test_connect(entry.data[CONF_HOST], entry.data[CONF_PORT])
-    except ConnectionError as err:
-        raise ConfigEntryNotReady from err
-
+    """Set up an LG soundbar using the platform's persistent connection."""
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
