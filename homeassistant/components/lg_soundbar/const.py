1ba06d0de2b345838aae1a7718dd9ed2cd5bb90f
MojoManagement <816086+MojoManagement@users.noreply.github.com>
2026-08-25 19:42:28 +0200

    refactor: own LG soundbar protocol lookup constants

--- homeassistant/components/lg_soundbar/const.py
+++ homeassistant/components/lg_soundbar/const.py
@@ -1,5 +1,55 @@
 """Constants for the LG Soundbar integration."""
 
 DOMAIN = "lg_soundbar"
-
 DEFAULT_PORT = 9741
+CONF_MODEL = "model"
+
+EQUALISERS = (
+    "Standard",
+    "Bass",
+    "Flat",
+    "Boost",
+    "Treble and Bass",
+    "User",
+    "Music",
+    "Cinema",
+    "Night",
+    "News",
+    "Voice",
+    "ia_sound",
+    "Adaptive Sound Control",
+    "Movie",
+    "Bass Blast",
+    "Dolby Atmos",
+    "DTS Virtual X",
+    "Bass Boost Plus",
+    "DTS X",
+    "AI Sound Pro",
+    "Clear Voice",
+    "Sports",
+    "Game",
+)
+
+FUNCTIONS = (
+    "Wi-Fi",
+    "Bluetooth",
+    "Portable",
+    "Aux",
+    "Optical",
+    "CP",
+    "HDMI",
+    "ARC",
+    "Spotify",
+    "Optical2",
+    "HDMI2",
+    "HDMI3",
+    "LG TV",
+    "Mic",
+    "Chromecast",
+    "Optical/HDMI ARC",
+    "LG Optical",
+    "FM",
+    "USB",
+    "USB2",
+    "E-ARC",
+)
